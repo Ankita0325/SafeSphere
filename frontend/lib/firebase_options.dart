bb1a86cd813c2bfb13c5aee5b84e88220b33cab6
Ankita <ankitar1979@gmail.com>
2026-07-10 19:43:21 +0530

    heatmap working

--- frontend/lib/firebase_options.dart
+++ frontend/lib/firebase_options.dart
@@ -1,16 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'demo-api-key',
-    appId: 'demo-app-id',
-    messagingSenderId: 'demo-sender-id',
-    projectId: 'women-safety-demo',
-    authDomain: 'women-safety-demo.firebaseapp.com',
-    storageBucket: 'women-safety-demo.appspot.com',
-  );
-
   static FirebaseOptions get currentPlatform {
-    return web;
+    return const FirebaseOptions(
+      apiKey: 'YOUR_API_KEY',
+      appId: 'YOUR_APP_ID',
+      messagingSenderId: 'YOUR_SENDER_ID',
+      projectId: 'YOUR_PROJECT_ID',
+      storageBucket: 'YOUR_STORAGE_BUCKET',
+    );
   }
 }
